7d93bf615d68e56bdf0767155eff616bdc523645
kimosabey <harshan.aiyappa@gmail.com>
2025-12-31 13:33:06 +0530

    chore: clean up duplicate files from folder rename

--- dataquarantine/config/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Config package initialization"""
-
-from dataquarantine.config.settings import settings
-from dataquarantine.config.logging_config import logger
-
-__all__ = ["settings", "logger"]
--- dataquarantine/streaming/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Streaming package initialization"""
-
-from dataquarantine.streaming.kafka_consumer import KafkaMessageConsumer
-from dataquarantine.streaming.kafka_producer import KafkaMessageProducer
-
-__all__ = [
-    "KafkaMessageConsumer",
-    "KafkaMessageProducer",
-]
